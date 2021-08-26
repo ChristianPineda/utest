b38ea2a1a6395de48e383db718081f036b78dfe5
elpincode <viajero06701@innovaccion.mx>
2021-08-26 13:39:23 -0500

    web gradiente

---
+++ lib/web_components/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Center(
+          child: Text('Degradados'),
+        )
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.blue,
+                Colors.red,
+              ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
